8a4e5c7bb166987aebaa63328f93a3ce4590906c
krisxb <995810143@qq.com>
2016-07-10 17:22:09 +0800

    360shop
    first

---
+++ 360shop/js/shop.js
@@ -0,0 +1,14 @@
+window.onload=function () {
+        var designWidth=768;
+        var fontSize=12;
+        var fontSize2=10;
+        function resize() {
+            var deviceWidth=document.documentElement.clientWidth;
+            var ratio=deviceWidth/designWidth;
+            var newfontSize=ratio<1?fontSize2:fontSize2*ratio;
+            // document.documentElement.style.fontSize=newfontSize+'px';
+            document.documentElement.style.fontSize=newfontSize+'px';
+        }
+        resize();
+        window.onresize=resize;
+ }
